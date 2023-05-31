63dc58982e486e39600d6308a85df67f9150c8b3
sdairs <sdairs@users.noreply.github.com>
2023-05-31 17:58:54 +0100

    very basic initial leaderboard

--- src/EndGameScene.js
+++ src/EndGameScene.js
@@ -5,6 +5,8 @@ export default class EndGameScene extends Phaser.Scene {
     name: '',
     id: ''
   };
+  top_10_url = new URL(`https://api.tinybird.co/v0/pipes/top_10_sessions.json`);
+  top_10_read_token = 'p.eyJ1IjogIjYwOTQyZDQ5LTU1NzYtNDIyMC04YTRjLTZhOTVlMmU1N2RlNCIsICJpZCI6ICI1ODRmMWJhZi1lZTc4LTQ5MmUtOWIxYS1iZjcyNjk4MGFkZjAifQ.xZJUmNrN0f9MFO91-i-eA9_iJH25nFYeNruTn55SqQI';
 
   constructor() {
     super({ key: "EndGameScene" });
@@ -16,15 +18,31 @@ export default class EndGameScene extends Phaser.Scene {
 
   create() {
 
+    const top_10_result = fetch(this.top_10_url, {
+      headers: {
+        Authorization: `Bearer ${this.top_10_read_token}`
+      }
+    })
+      .then(r => r.json())
+      .then(r => this.buildTopTen(r))
+      .catch(e => e.toString())
+
+    const base_y = 155;
+
+    this.buildHomeButton(base_y);
+    this.buildRetryButton(base_y);
+  }
+
+  buildHomeButton(base_y) {
     const homeButton = this.add.graphics();
     homeButton.fillStyle(0x1fcc83, 1);
-    homeButton.fillRect(150, 235, 100, 50);
+    homeButton.fillRect(150, base_y, 100, 50);
     homeButton.setInteractive(
-      new Phaser.Geom.Rectangle(150, 235, 100, 50),
+      new Phaser.Geom.Rectangle(150, base_y, 100, 50),
       Phaser.Geom.Rectangle.Contains
     );
 
-    this.add.text(170, 247.5, "Home", {
+    this.add.text(170, base_y + 12.5, "Home", {
       fontSize: "24px",
       color: "#ffffff",
     });
@@ -36,16 +54,18 @@ export default class EndGameScene extends Phaser.Scene {
       },
       this
     );
+  }
 
+  buildRetryButton(base_y) {
     const retryButton = this.add.graphics();
     retryButton.fillStyle(0x1fcc83, 1);
-    retryButton.fillRect(150, 290, 100, 50);
+    retryButton.fillRect(150, base_y + 55, 100, 50);
     retryButton.setInteractive(
-      new Phaser.Geom.Rectangle(150, 290, 100, 50),
+      new Phaser.Geom.Rectangle(150, base_y + 55, 100, 50),
       Phaser.Geom.Rectangle.Contains
     );
 
-    this.add.text(165, 303, "Retry", {
+    this.add.text(165, base_y + 55 + 12.5, "Retry", {
       fontSize: "24px",
       color: "#ffffff",
     });
@@ -57,7 +77,25 @@ export default class EndGameScene extends Phaser.Scene {
       },
       this
     );
+  }
 
+  buildTopTen(top10_result) {
+    let y = 300;
+    let position = 1;
+
+    this.add.text(120, y, 'Leaderboard', {
+      fontSize: "24px",
+      color: "#ffffff",
+    });
+    y += 46;
+    top10_result.data.forEach(entry => {
+      this.add.text(120, y, `#${position < 10 ? '0' + position : position} | ${entry.score} | ${entry.name}`, {
+        fontSize: "18px",
+        color: "#ffffff",
+      });
+      y += 20;
+      position++;
+    });
 
   }
 }
