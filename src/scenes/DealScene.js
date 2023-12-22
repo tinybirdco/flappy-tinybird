a6a3dfbfc6155bc996d93b0753f6553c977fb295
DavidMarguliesTB <david.margulies@tinybird.co>
2023-12-22 16:33:19 -0500

    upd message

--- src/scenes/DealScene.js
+++ src/scenes/DealScene.js
@@ -32,7 +32,7 @@ export default class DealScene extends Phaser.Scene {
         const text = this.add.text(
             this.cameras.main.width / 2,
             100,
-            "Flappy is tired of dying :(\n\nBuy this power-up to\nslow down the game!",
+            "Flappy is tired of dying :(\n\nPurchase this power-up to\nactivate slow-mo!",
             {
                 align: 'center',
             }
