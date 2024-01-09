1341c3230778265d91c64f06c168df4fd50341c4
Joe <krawiecpj@gmail.com>
2024-01-09 13:01:33 -0500

    serverless express function

---
+++ api/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { Kafka } = require('kafkajs');
+const cors = require('cors');
+
+const kafka = new Kafka({
+    clientId: process.env.KAFKA_CLIENT_ID,
+    brokers: [process.env.KAFKA_BROKER_URL],
+    ssl: true,
+    sasl: {
+        mechanism: 'plain',
+        username: process.env.KAFKA_API_KEY,
+        password: process.env.KAFKA_API_SECRET,
+    },
+});
+
+const producer = kafka.producer();
+const app = express();
+
+app.use(express.json());
+app.use(cors());    
+
+app.post('/sendToKafka', async (req, res) => {
+    const payload = req.body;
+    const topic = 'demo_flappy';
+
+    try {
+        await producer.connect();
+        await producer.send({
+            topic,
+            messages: [
+                { key: 'dev_jk', value: JSON.stringify(payload) },
+            ],
+        });
+
+        res.json({ status: 'Message sent to Kafka' });
+    } catch (error) {
+        console.error('Error sending message to Kafka:', error);
+        res.json({ error: error.message });
+    } finally {
+        await producer.disconnect();
+    }
+});
+
+module.exports = app; // Export the app object
